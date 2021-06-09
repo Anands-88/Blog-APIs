9b45fbde143273a55cc91af0923e3d08fd3062f3
Anands-88 <anandsholla8@gmail.com>
2021-06-09 22:12:38 +0530

    Add modified files

--- config/urls.py
+++ config/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path('useradmin/', admin.site.urls),
     path('api/v1/', include('posts.urls')), # new
+    path('api-auth/',include('rest_framework.urls')) # New chapter
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,15 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
+from rest_framework.settings import perform_import
 from .models import * 
 from .serializers import *
+from .permissions import IsAuthorOrReadOnly # new
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
