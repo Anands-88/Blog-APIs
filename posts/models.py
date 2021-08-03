be2d179fbc7ace4fdcd9a0d16c56337d5226a7de
Anands-88 <anandsholla8@gmail.com>
2021-08-03 22:54:16 +0530

    Add updated files

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.deletion import CASCADE
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
